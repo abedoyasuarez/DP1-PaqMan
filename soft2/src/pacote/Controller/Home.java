752ec8934ba84f9aed6333734c3e2f555e48dfe3
JulioCR <juliocr1991@gmail.com>
2013-05-19 16:15:50 -0500

    pruebaza

--- soft2/src/pacote/Controller/Home.java
+++ soft2/src/pacote/Controller/Home.java
@@ -1,5 +1,5 @@
 package pacote.Controller;
-
+//asdfssdfa
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
