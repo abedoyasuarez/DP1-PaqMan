e1f369a9f77744e7861077b2bbd1117b4f92c765
dsalas <salas.diego@pucp.pe>
2013-05-19 18:32:55 -0500

    Comparators

---
+++ soft2/src/paqman/Model/Bean/pedidosComparator.java
@@ -0,0 +1,11 @@
+package paqman.Model.Bean;
+
+import java.util.Comparator;
+
+public class pedidosComparator implements Comparator<Pedido> {
+	@Override
+	public int compare(Pedido p1, Pedido p2) {
+		return 0;
+	}
+}
+
---
+++ soft2/src/paqman/Model/Bean/vehiculoComparator.java
@@ -0,0 +1,11 @@
+package paqman.Model.Bean;
+
+import java.util.Comparator;
+
+public class vehiculoComparator implements Comparator<Vehiculo> {
+	@Override
+	public int compare(Vehiculo v1, Vehiculo v2) {
+		return 1;
+	}
+}
+
