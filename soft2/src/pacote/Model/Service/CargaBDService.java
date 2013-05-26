fd089586294de11ec4a02b1df44f5223def6daa6
JulioCR <juliocr1991@gmail.com>
2013-05-26 12:27:11 -0500

    Servicio de carga de BD

---
+++ soft2/src/pacote/Model/Service/CargaBDService.java
@@ -0,0 +1,24 @@
+package pacote.Model.Service;
+
+import pacote.Model.DAO.Conexion;
+import pacote.Model.DAO.DAO_CargaBD;
+
+public class CargaBDService {
+	
+	private static DAO_CargaBD dao_cargabd = new DAO_CargaBD();
+	private static Conexion conexion= new Conexion();
+	
+	public void buscaDataSimulacionDia(){
+		
+		try{
+			conexion.abrirConexion();
+			dao_cargabd.setConexion(conexion);
+			dao_cargabd.cargarDatos();
+			conexion.cerrarConexion();
+		}catch(Exception e){
+			e.printStackTrace();			
+		}
+		
+	}
+	
+}
